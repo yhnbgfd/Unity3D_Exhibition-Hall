5046e58ec371e074c9dbd068609ce836d1288221
HuangShuhang <yhnbgfd@gmail.com>
2013-12-27 00:48:24 +0800

    MouseLook
    getMouseY还有问题。算法不准。

--- Assets/Plugins/script/LeapMotion/LMGesture.cs
+++ Assets/Plugins/script/LeapMotion/LMGesture.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class LMGesture {
 	LeapMotion lm;
+	private float mouseX = 0.0f;
+	private float mouseY = 0.0f;
+	private float introY = 0.0f;
 
 	public LMGesture () {
 		lm = new LeapMotion();
@@ -15,17 +18,49 @@ public class LMGesture {
 
 	public float getMouseX() {
 		Vector3 lookPosition = lm.FingertipPosition();
-		//Debug.Log(lookPosition.x);
-
-
-		return 0.0f;
+		if(lookPosition.x > 0)
+		{
+			if(mouseX < 0.1f )
+			{
+				mouseX += 0.01f;
+			}
+			else if (mouseX >= 0.1f)
+			{
+				mouseX = 0.1f;
+			}
+		}
+		else if(lookPosition.x < 0)
+		{
+			if(mouseX > -0.1f )
+			{
+				mouseX -= 0.01f;
+			}
+			else if (mouseX <= -0.1f)
+			{
+				mouseX = -0.1f;
+			}
+		}
+		return mouseX;//-0.1 ~ 0.1(0.2)
 	}
 
 	public float getMouseY() {
+		if(lm.HandEnter())
+		{
+			introY = lm.FingertipPosition().y;
+			Debug.Log("HandEnter_"+introY);
+		}
+		introY = 200.0f;
 		Vector3 lookPosition = lm.FingertipPosition();
 
-
-
-		return 0.0f;
+		Debug.Log(lookPosition.y - introY);
+		if(lookPosition.y - introY > 0)
+		{
+			return 0.05f;
+		}
+		else if(lookPosition.y - introY < 0)
+		{
+			return -0.05f;
+		}
+		return mouseY;//-0.1 ~ 0.1(0.2)
 	}
 }
--- Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -38,21 +38,20 @@ public class MouseLook : MonoBehaviour {
 		{
 			//float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 			float rotationX = transform.localEulerAngles.y + lg.getMouseX() * sensitivityX;
-
 			//rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY += lg.getMouseY() * sensitivityY;
-
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			//rotationY += lg.getMouseY() * sensitivityY;
+			//rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
 		else if (axes == RotationAxes.MouseX)
 		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+			//transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
 		}
 		else
 		{
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			//rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY += lg.getMouseY() * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
