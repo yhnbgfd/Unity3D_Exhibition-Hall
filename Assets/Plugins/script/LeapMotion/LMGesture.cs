0c5f97e7ca4296030c1bee31c1f6d1779a2ffa2c
HuangShuhang <yhnbgfd@gmail.com>
2013-12-28 22:45:48 +0800

    改进x轴视角算法。

--- Assets/Plugins/script/LeapMotion/LMGesture.cs
+++ Assets/Plugins/script/LeapMotion/LMGesture.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class LMGesture {
-	LeapMotion lm;
+	private LeapMotion lm;
 	private float mouseX = 0.0f;
 	private float mouseY = 0.0f;
 	private float introY = 0.0f;
@@ -18,27 +18,49 @@ public class LMGesture {
 
 	public float getMouseX() {
 		Vector3 lookPosition = lm.FingertipPosition();
+		Debug.Log(lookPosition.x);
 		if(lookPosition.x > 0)
 		{
-			if(mouseX < 0.1f )
+			if(lookPosition.x > 100)
 			{
-				mouseX += 0.01f;
+				if(mouseX < 0.1f )
+				{
+					mouseX += 0.01f;
+				}
+				else if (mouseX >= 0.1f)
+				{
+					mouseX = 0.1f;
+				}
 			}
-			else if (mouseX >= 0.1f)
+			else
 			{
-				mouseX = 0.1f;
+				if(mouseX > 0)
+				{
+					mouseX -= 0.01f;
+				}
 			}
 		}
 		else if(lookPosition.x < 0)
 		{
-			if(mouseX > -0.1f )
+			if(lookPosition.x < -100)
 			{
-				mouseX -= 0.01f;
+				if(mouseX > -0.1f )
+				{
+					mouseX -= 0.01f;
+				}
+				else if (mouseX <= -0.1f)
+				{
+					mouseX = -0.1f;
+				}
 			}
-			else if (mouseX <= -0.1f)
+			else
 			{
-				mouseX = -0.1f;
+				if(mouseX < 0)
+				{
+					mouseX += 0.01f;
+				}
 			}
+
 		}
 		return mouseX;//-0.1 ~ 0.1(0.2)
 	}
@@ -47,7 +69,7 @@ public class LMGesture {
 		if(lm.HandEnter())
 		{
 			introY = lm.FingertipPosition().y;
-			Debug.Log("HandEnter_"+introY);
+			//Debug.Log("HandEnter_"+introY);
 		}
 		introY = 200.0f;
 		Vector3 lookPosition = lm.FingertipPosition();
@@ -56,7 +78,7 @@ public class LMGesture {
 			return 0.0f;
 		}
 
-		Debug.Log(lookPosition.y - introY);
+		//Debug.Log(lookPosition.y - introY);
 		if(lookPosition.y - introY > 0)
 		{
 			return 0.05f;
