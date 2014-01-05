e37fb2cdac16b368c81531ae442e08bd1e4f7389
HuangShuhang <yhnbgfd@gmail.com>
2014-01-05 20:26:58 +0800

    添加自动寻路

---
+++ Assets/Plugins/script/NavMeshTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshTest : MonoBehaviour {
+	private NavMeshAgent man;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		man = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		man.SetDestination(target.position);
+	}
+}
